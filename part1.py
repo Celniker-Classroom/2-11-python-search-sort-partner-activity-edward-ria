e19e3f75eda4b6281c98c3afcaef562cc8bee5c7
aie6952 <257592232+aie6952@users.noreply.github.com>
2026-02-19 17:13:57 +0000

    part2update

--- part1.py
+++ part1.py
@@ -6,7 +6,7 @@ list = [] #name your list and make sure it is empty!
 
 # Generates a list of 5 or 10 random integers between 1 and 50 inclusive.
 def genNumber():
-    for num in range(5): #for loop appends 5 numbers to your list, but make sure you name your variable
+    for num in range(10): #for loop appends 5 numbers to your list, but make sure you name your variable
         list.append(randint(1,50)) #this adds a random number between 1-50 to the list
 
 genNumber()
