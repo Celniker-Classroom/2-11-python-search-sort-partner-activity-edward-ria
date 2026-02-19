adf96ebc08a8d1f9fcc9bb10b007ef03e8226779
aie6952 <257592232+aie6952@users.noreply.github.com>
2026-02-19 17:38:59 +0000

    part2b

--- part2.py
+++ part2.py
@@ -2,7 +2,9 @@ from random import randint #This line imports the randint function from the rand
 
 
 list = [] #name your list and make sure it is empty!
-randomNumber = randint(1,50)
+randomNumber = randint(1,50) #the random number that we want to search for in the list
+comparisons = 0
+found = False
 
 # Generates a list of 5 or 10 random integers between 1 and 50 inclusive.
 def genNumber():
@@ -13,9 +15,13 @@ def genNumber():
 genNumber()
 print("Generated List:",list) #print the list!
 print("") #paragraph for nice spacing/visual looks
-print("Searching for number:",randomNumber)
+print("Searching for number:",randomNumber) #searching for the random number in list
 
-if randomNumber in list:
-    print("Number",randomNumber,"found in the list!")
-else:
-    print("Number",randomNumber,"not found in the list.")
+for numbers in list:  # Name your variable in the for loop
+    comparisons += 1  # Increment the counter for each comparison
+    if numbers == randomNumber:
+        found = True  # Set found to True if the number is in the list
+        print("Number",randomNumber,"found in the list after",comparisons,"comparisons!")
+        break  # Exit the loop early if the number is found
+    else:
+        print("Number",randomNumber,"not found in the list after",comparisons,"comparisons!")
