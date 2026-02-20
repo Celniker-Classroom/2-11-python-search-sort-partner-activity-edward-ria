36c7b0a5f0b39aa7e0cde14cca9559dec35fd187
aie6952 <257592232+aie6952@users.noreply.github.com>
2026-02-20 17:01:29 +0000

    part2ab

--- part3f.py
+++ part3f.py
@@ -1,24 +1,37 @@
 from random import randint #This line imports the randint function from the random module. The randint function generates a random integer between two specified values
 
-
 list = [] #name your list and make sure it is empty!
-randomNumber = int(input("What number do you want to search for in the list?")) #user input number to search for
-numRange = int(input("How many numbers do you want to generate?")) #how big the list/how many numbers to genreate
 maxNumber = int(input("What is the biggest number that should be generated?")) #what the max number is for the random numbers
-minNumber = int(input("What is the smallest number that should be able to be generated?"))
+minNumber = int(input("What is the smallest number that should be able to be generated?")) #what the minimum number is for the random number range
+numRange = int(input("How many numbers do you want to generate?")) #how big the list/how many numbers to genreate 
+randomNumber = int(input("What number do you want to search for in the list?")) #user input number to search for
 comparisons = 0 #initial comparisons
-found = False
+percentage = 0 #initial percent chance
 
 # Generates a list of 5 or 10 random integers between 1 and 50 inclusive.
 def genNumber():
-    for num in range(numRange): #for loop appends 5 numbers to your list, but make sure you name your variable
-        list.append(randint(1,maxNumber)) #this adds a random number between 1-50 to the list
+    for num in range(numRange): #now puts numbers in range of the user input
+        list.append(randint(minNumber,maxNumber)) #adds a number based on the minimum and maximum user input variable
+
+# get the percent chance of finding the number within the list
+if randomNumber < minNumber: # if the chosen number is smaller than the minimum number
+    percentage = 0 #then there is a 0% chance of finding it within the list
+elif randomNumber > maxNumber: # otherwise if the chosen number is bigger than the maximum number
+    percentage = 0 # then there is a 0% chance of finding it within the list
+else: # if the chosen number is between smallest/biggest number, then find the percentage
+    possibleNums = (maxNumber - minNumber) + 1 #total numbers that will be on the list
+    probMiss = (possibleNums - 1) / possibleNums #the chance that the number won't be picked in the first scan
+    probFailure = probMiss ** numRange # the chance that the number won't even be found on the list
+    percentage = round((1 - probFailure) * 100, 2) # using round function to find percentage
 
 
-genNumber()
-print("Generated List:",list) #print the list!
+genNumber() #call the function
+print("Generated List:",list) #print the list
 print("") #paragraph for nice spacing/visual looks
+print(randomNumber,"has a",percentage,"% chance to be found in the list")
 print("Searching for number:",randomNumber) #searching for the random number in list
+import time
+time.sleep(3) #for dramatic effect 
 
 if randomNumber in list:
     print("Number",randomNumber,"found in the list!") #if found
